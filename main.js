f50a7a21c6b7ac363e0fdfc1c13300b336eb2e6c
Htun-Aung-Kyaw <htunaung7321@gmail.com>
2025-02-19 13:50:32 +0630

    Scroll Reveal Effect Added

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log("This is scroll reveal effect with js");
+
+let cards = document.querySelectorAll(".card");
+console.log(cards);
+
+window.addEventListener("load", () => {
+    cards.forEach(card=>{
+        card.classList.add("reveal");
+    });
+});
+
+window.addEventListener("scroll",scrollEventListener);
+
+function scrollEventListener()
+{
+    console.log("Event Listener Called.");
+
+    cards.forEach((card, index) => {
+
+        let windowHeight = window.innerHeight;
+        let sectionRectTop = card.getBoundingClientRect().top;
+
+        if(sectionRectTop<windowHeight && card.classList.contains("reveal")){
+            card.classList.add("active");
+        }
+        else
+            card.classList.remove("active");
+    });
+}
